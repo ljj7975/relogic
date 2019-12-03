cbee5ed9b702f24dc2d319f6e3ea01cfa1ca0bab
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-12-03 17:28:42 -0500

    sample scripts for POS

--- tests/experiments/pos.sh
+++ tests/experiments/pos.sh
@@ -1,3 +1,5 @@
+date
+
 CUDA_VISIBLE_DEVICES=1
 output_dir=saves/ner/conll2006/pos/es/test
 
@@ -9,19 +11,21 @@ python -u -m relogic.main \
 --output_dir ${output_dir} \
 --encoder_type bert \
 --bert_model bert-base-multilingual-cased  \
---raw_data_path ${DATA_DIR}/CONLL_POS/el/ \
+--raw_data_path ${DATA_DIR}/CONLL_POS/de/ \
 --label_mapping_path data/preprocessed_data/conll0607_pos_label_mapping.json \
 --model_name default \
 --no_cuda \
 --learning_rate 1e-5 \
---train_batch_size 3 \
---test_batch_size 3 \
+--train_batch_size 6 \
+--test_batch_size 6 \
 --epoch_number 3 \
---eval_dev_every 5 \
+--eval_dev_every 1000 \
 --pretokenized \
 --metrics accuracy \
---early_stop_at 5 \
+--early_stop_at 19608 \
 --config_file configurations/mbert_config.json
+
+date
 
 python -u -m relogic.main \
 --mode eval \
@@ -29,3 +33,5 @@ python -u -m relogic.main \
 --no_cuda
 
 rm -r ${output_dir}
+
+date
